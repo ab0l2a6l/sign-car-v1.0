363df3b5f57f7881b4b13428ab088be753fde27d
abolfazl <a.b.f.m.98@gmail.com>
2023-06-03 22:38:28 +0430

    ezafe kardan delete , update , findByAll (albate hanoz moshkel dare)

--- src/model/repository/CarDBDAO.java
+++ src/model/repository/CarDBDAO.java
@@ -5,23 +5,25 @@ import model.entity.Car;
 import java.sql.*;
 import java.util.List;
 
-public class CarDBDAO implements CarDAOWrite , CarDAORead{
-    String url = "jdbc:mysql://localhost:3306/firstdb";
-    String username = "root";
-    String password = "Am311865186";
+public class CarDBDAO implements CarDAOWrite, CarDAORead {
+    final String url = "jdbc:mysql://localhost:3306/firstdb";
+    final String username = "root";
+    final String password = "Am311865186";
+    private String query;
 
-    Statement statement;
-    Connection connection;
+
+    private Connection connection;
+    private Statement statement;
+
 
     @Override
     public void save(Car car) {
         try {
-            connection = DriverManager.getConnection(url , username , password);
+            connection = DriverManager.getConnection(url, username, password);
             System.out.println(connection + " connected");
+            statement = connection.createStatement();
 
-            statement =connection.createStatement();
-
-            String query = "insert into Car values ("+ car.getId() + ","+ car.getModel()+")";
+            query = "insert into Car values (" + car.getId() + "," + car.getModel() + ")";
             statement.executeUpdate(query);
 
             statement.close();
@@ -34,12 +36,45 @@ public class CarDBDAO implements CarDAOWrite , CarDAORead{
 
     @Override
     public void delete(long id) {
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println(connection + " connected");
+            statement = connection.createStatement();
 
+            query = "delete from car where id = " + id;
+            statement.executeUpdate(query);
+
+            statement.close();
+            connection.close();
+            System.out.println("disconnected");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void update(Car car) {
+        try {
 
+
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println(connection + " connected");
+            statement = connection.createStatement();
+
+            //update firstdb.car
+            // set model = 'pride'
+            // where id = 222
+
+            query = "update car set model =" + car.getModel() + " where id = "+ car.getId();
+            statement.executeUpdate(query);
+
+
+            statement.close();
+            connection.close();
+            System.out.println("disconnected");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
@@ -47,12 +82,12 @@ public class CarDBDAO implements CarDAOWrite , CarDAORead{
         Car car = new Car();
         try {
 
-            connection = DriverManager.getConnection(url , username , password);
+            connection = DriverManager.getConnection(url, username, password);
             System.out.println(connection + " connected");
 
             statement = connection.createStatement();
 
-            String query = "select * from car ";
+            query = "select * from car ";
             ResultSet resultSet = statement.executeQuery(query);
 
             while (resultSet.next()) {
@@ -75,6 +110,31 @@ public class CarDBDAO implements CarDAOWrite , CarDAORead{
 
     @Override
     public List<Car> findByAll() {
+
+        try {
+
+
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println(connection + " connected");
+            statement = connection.createStatement();
+
+            query = "select * from car";
+            ResultSet resultSet = statement.executeQuery(query);
+
+//            while (resultSet.next()) {
+//                System.out.println("id: " + resultSet.getInt("id"));
+//                System.out.println("model: " + resultSet.getString("model"));
+//            }
+
+
+            statement.close();
+            connection.close();
+            System.out.println("disconnected");
+
+        }catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
         return null;
     }
 }
