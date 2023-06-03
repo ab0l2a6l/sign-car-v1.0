1a3cda27bbc8a0184ce8aab28aad80feccb9effa
abolfazl <a.b.f.m.98@gmail.com>
2023-06-03 18:03:54 +0430

    ezafe kardan search b DB

--- src/model/repository/CarDBDAO.java
+++ src/model/repository/CarDBDAO.java
@@ -2,10 +2,7 @@ package model.repository;
 
 import model.entity.Car;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.List;
 
 public class CarDBDAO implements CarDAOWrite , CarDAORead{
@@ -13,13 +10,16 @@ public class CarDBDAO implements CarDAOWrite , CarDAORead{
     String username = "root";
     String password = "Am311865186";
 
+    Statement statement;
+    Connection connection;
+
     @Override
     public void save(Car car) {
         try {
-            Connection connection = DriverManager.getConnection(url , username , password);
+            connection = DriverManager.getConnection(url , username , password);
             System.out.println(connection + " connected");
 
-            Statement statement =connection.createStatement();
+            statement =connection.createStatement();
 
             String query = "insert into Car values ("+ car.getId() + ","+ car.getModel()+")";
             statement.executeUpdate(query);
@@ -28,7 +28,7 @@ public class CarDBDAO implements CarDAOWrite , CarDAORead{
             connection.close();
             System.out.println("disconnected");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
     }
 
@@ -44,7 +44,33 @@ public class CarDBDAO implements CarDAOWrite , CarDAORead{
 
     @Override
     public Car findById(long id) {
-        return null;
+        Car car = new Car();
+        try {
+
+            connection = DriverManager.getConnection(url , username , password);
+            System.out.println(connection + " connected");
+
+            statement = connection.createStatement();
+
+            String query = "select * from car ";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                if (id == resultSet.getInt("id")) {
+                    long tempId = resultSet.getInt("id");
+                    String model = resultSet.getString("model");
+                    car.setId(tempId);
+                    car.setModel(model);
+                }
+            }
+            statement.close();
+            connection.close();
+            System.out.println("disconnected");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return car;
     }
 
     @Override
